7b569f8e73d5bdee21031e634bba91b26f277295
Marius S. Riseth <marius.s.riseth@pm.me>
2023-11-08 13:47:56 +0100

    knapp

--- riktig.js
+++ riktig.js
@@ -1,1 +1,10 @@
-function svar(riktig, feil) {}
+function Spm1(svar, button) {
+   if (svar === "riktig") {
+      button.classList.add("riktig");
+      //   button.disabled = true;
+      document.getElementById("videre").style.display = "block";
+   } else {
+      button.classList.add("feil");
+      //   button.disabled = true;
+   }
+}
