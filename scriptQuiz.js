b3c837e4540c629b57b3a726e85c36a24660c441
Marius S. Riseth <marius.s.riseth@pm.me>
2023-11-10 11:20:26 +0100

    Separer quiz

---
+++ scriptQuiz.js
@@ -0,0 +1,40 @@
+let liv = 6;
+document.getElementById("liv").textContent = liv;
+function Spm1(svar, button) {
+   const buttons = document.querySelectorAll(".flex button");
+
+   buttons.forEach(function (btn) {
+      btn.classList.remove("riktig", "feil");
+      btn.disabled = false;
+   });
+
+   if (svar === "riktig") {
+      button.classList.add("riktig");
+      button.disabled = true;
+      document.getElementById("videre").style.display = "block";
+   } else {
+      button.classList.add("feil");
+      liv--;
+      document.getElementById("liv").textContent = liv;
+   }
+}
+
+function Spm1(svar, button) {
+   const buttons = document.querySelectorAll(".flex button");
+
+   if (liv > 0) {
+      buttons.forEach(function (btn) {
+         btn.classList.remove("riktig", "feil");
+         btn.disabled = true;
+      });
+
+      if (svar === "riktig") {
+         button.classList.add("riktig");
+         document.getElementById("videre").style.display = "block";
+      } else {
+         button.classList.add("feil");
+         liv--;
+         document.getElementById("liv").textContent = liv;
+      }
+   }
+}
