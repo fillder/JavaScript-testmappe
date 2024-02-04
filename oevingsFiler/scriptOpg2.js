f777fb0fe168d8eb5ee5db0543ca0fdf58459863
Marius S. Riseth <marius.s.riseth@pm.me>
2024-02-04 19:54:42 +0100

    321

---
+++ oevingsFiler/scriptOpg2.js
@@ -0,0 +1,6 @@
+let vafler = 0;
+let ingredienser = 10;
+let vaflerTekst = document.getElementById("antallVafler");
+let ingredienserTekst = document.getElementById("antallIngredienser");
+
+function lagVafler() {}
