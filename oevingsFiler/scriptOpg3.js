2136f051cd4ffa2d4af41474144af0f0bc967fd5
Marius S. Riseth <marius.s.riseth@pm.me>
2024-02-04 20:16:01 +0100

    432

---
+++ oevingsFiler/scriptOpg3.js
@@ -0,0 +1,5 @@
+let vafler = 10;
+let vaflerTekst = document.getElementById("antallVafler");
+let varselTekst = document.getElementById("varselTekst");
+
+function selgVafler() {}
---
+++ oevingsFiler/scriptOpg4.js
@@ -0,0 +1,6 @@
+let pris = 8;
+let varselTekst = document.getElementById("varselTekst");
+let salgsTekst = document.getElementById("salgsTekst");
+
+function prisOpp() {}
+function prisNed() {}
