4e344cb82c3d55cf46c32ef2c04c53cbac28cadd
Marius S. Riseth <marius.s.riseth@pm.me>
2024-03-05 17:41:54 +0100

    test

---
+++ javaScriptSertifisering/script.js
@@ -0,0 +1,13 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+/*  Vi kan legge til en array i variabelen ved hjelp av klammeparenteser.
+    let inventory = ["stick","dagger","sword"];*/
+let inventory = ["stick"];
+//  querySelector("") ser etter det første eksemplaret av nevnt element, nyttig for klasser og h1-tagg
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
