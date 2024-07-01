c5b4551ab841ec7382d138a1a23fb466826b08f1
Marius S. Riseth <marius.s.riseth@pm.me>
2024-07-01 22:01:37 +0200

    GameComplete

--- FCC_rolePlayingGame/RPG_script.js
+++ FCC_rolePlayingGame/RPG_script.js
@@ -4,8 +4,7 @@ let gold = 50;
 let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick"];
-
+let inventory = [" stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -17,10 +16,10 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
-   { name: "stick", power: 5 },
-   { name: "dagger", power: 30 },
-   { name: "claw hammer", power: 50 },
-   { name: "sword", power: 100 },
+   { name: " stick", power: 5 },
+   { name: " dagger", power: 30 },
+   { name: " claw hammer", power: 50 },
+   { name: " sword", power: 100 },
 ];
 const monsters = [
    { name: "slime", level: 2, health: 15 },
@@ -52,6 +51,30 @@ const locations = [
       buttonFunctions: [attack, dodge, goTown],
       text: "You are fighting a monster.",
    },
+   {
+      name: "kill monster",
+      buttonText: ["Go to town square", "Go to town square", "Go to town square"],
+      buttonFunctions: [goTown, goTown, easterEgg],
+      text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+   },
+   {
+      name: "lose",
+      buttonText: ["REPLAY?", "REPLAY?", "REPLAY?"],
+      buttonFunctions: [restart, restart, restart],
+      text: "You die. &#x2620;",
+   },
+   {
+      name: "win",
+      buttonText: ["REPLAY?", "REPLAY?", "REPLAY?"],
+      buttonFunctions: [restart, restart, restart],
+      text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+   },
+   {
+      name: "easter egg",
+      buttonText: ["2", "8", "Go to town square?"],
+      buttonFunctions: [pickTwo, pickEight, goTown],
+      text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+   },
 ];
 
 // initialize buttons
@@ -60,13 +83,14 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+   monsterStats.style.display = "none";
    button1.innerText = location.buttonText[0];
    button2.innerText = location.buttonText[1];
    button3.innerText = location.buttonText[2];
    button1.onclick = location.buttonFunctions[0];
    button2.onclick = location.buttonFunctions[1];
    button3.onclick = location.buttonFunctions[2];
-   text.innerText = location.text;
+   text.innerHTML = location.text;
 }
 function goTown() {
    update(locations[0]);
@@ -81,14 +105,58 @@ function goFight() {
    update(locations[3]);
    // Bruker verdien til en variabel for å hente riktig indeksert monster sine stats
    monsterHealth = monsters[fighting].health;
-   monsterName.innerText = monsters[fighting].name;
+   monsterName.innerText = capitalizeFirstLetter(monsters[fighting].name);
    monsterHealthText.innerText = monsterHealth;
-   monsterStats.style.display = "block";
+   monsterStats.style.display = "flex";
+}
+function capitalizeFirstLetter(string) {
+   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 function attack() {
    // Bruker verdien til en variabel for å hente riktig indeksert monster sitt navn
    text.innerText = "The " + monsters[fighting].name + " attacks.";
+   // Bruker verdien til en variabel for å hente riktig indeksert våpennavn
+   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+   health -= getMonsterAttackValue(monsters[fighting].level);
+   // Legger til XP som en tilfeldig ektra skade våpenet gjør mot finenden
+   if (isMonsterHit()) {
+      monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+   } else {
+      text.innerText += " You miss.";
+   }
+   healthText.innerText = health;
+   monsterHealthText.innerText = monsterHealth;
+   if (health <= 0) {
+      lose();
+   } else if (monsterHealth <= 0) {
+      if (fighting === 2) {
+         winGame();
+      } else {
+         defeatMonster();
+      }
+   }
+   if (Math.random() <= 0.1 && inventory.length !== 1) {
+      text.innerText += " Your " + inventory.pop() + " breaks.";
+      currentWeaponIndex--;
+   }
 }
+function isMonsterHit() {
+   return Math.random() > 0.2 || health < 20;
+}
+function getMonsterAttackValue(level) {
+   const hit = level * 5 - Math.floor(Math.random() * xp);
+   if (hit > 0) {
+      return hit;
+   } else {
+      return 0;
+   }
+}
+/* 1. Her kan vi bruke en ternary operator. Den skrives:
+      2. condition ? expressionIfTrue : expressionIfFalse 
+      3. Hvis "hit" er over 0, returner "hit", hvis ikke returner 0 
+      
+      return hit > 0 ? hit : 0*/
+
 function dodge() {}
 
 function fightSlime() {
@@ -103,7 +171,16 @@ function fightDragon() {
    fighting = 2;
    goFight();
 }
-
+function defeatMonster() {
+   gold += Math.floor(monsters[fighting].level * 6.7);
+   xp += monsters[fighting].level;
+   goldText.innerText = gold;
+   xpText.innerText = xp;
+   update(locations[4]);
+}
+function lose() {
+   update(locations[5]);
+}
 function buyHealth() {
    if (gold >= 10) {
       gold -= 10;
@@ -146,4 +223,53 @@ function sellWeapon() {
    } else {
       text.innerText = "Don't sell your only weapon!";
    }
+}
+function lose() {
+   update(locations[5]);
+}
+function winGame() {
+   update(locations[6]);
+}
+function easterEgg() {
+   update(locations[7]);
+}
+function pickTwo() {
+   pick(2);
+}
+function pickEight() {
+   pick(8);
+}
+function pick(guess) {
+   const numbers = [];
+   while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
+   }
+   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+   for (let i = 0; i < 10; i++) {
+      text.innerText += numbers[i] + "\n";
+   }
+   if (numbers.includes(guess)) {
+      text.innerText += "Right! You win 20 gold!";
+      gold += 20;
+      goldText.innerText = gold;
+   } else {
+      text.innerText += "Wrong! You lose 10 health!";
+      health -= 10;
+      healthText.innerText = health;
+      if (health <= 0) {
+         lose();
+      }
+   }
+}
+
+function restart() {
+   xp = 0;
+   health = 100;
+   gold = 50;
+   currentWeaponIndex = 0;
+   inventory = ["stick"];
+   goldText.innerText = gold;
+   healthText.innerText = health;
+   xpText.innerText = xp;
+   goTown();
 }
