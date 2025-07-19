9ce4ff7d25a1d05d698ad007788b04c7e1448f32
Marius S. Riseth <marius.s.riseth@pm.me>
2025-07-19 20:43:19 +0200

    Konsilidering

---
+++ javaScriptKurs_2024/TycoonSpillEksempler/script/script.js
@@ -0,0 +1,9 @@
+let penger = 100
+penger = "du er blakk"
+
+const pengerTekst = document.getElementById("bareEndreDette");
+pengerTekst = "Dette går ikke"
+console.log(pengerTekst)
+
+pengerTekst.textContent = penger
+pengerTekst
